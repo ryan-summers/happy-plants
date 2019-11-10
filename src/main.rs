5f44c646cbb4891893f5508495c7db7e88ecdf35
Ryan Summers <summers.ryan.m@gmail.com>
2019-11-10 13:41:57 +0100

    Adding periodic timer

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,18 @@
 // extern crate panic_itm; // logs messages over ITM; requires ITM support extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 // use cortex_m_semihosting::{hprintln};
 
-extern crate panic_halt;
+extern crate panic_semihosting;
+
+#[macro_use(block)]
+extern crate nb;
 
 use core::fmt::Write;
 use cortex_m_rt::entry;
 use si7021::Si7021;
 
 use nrf52840_hal::{
+    prelude::*,
+    timer::{TimerExt},
     uarte,
     twim,
     uarte::{Parity, Baudrate},
@@ -21,16 +26,15 @@ use nrf52840_hal::{
     nrf52840_pac::Peripherals
 };
 
-
 #[entry]
 fn main() -> ! {
 
     let peripherals = Peripherals::take().unwrap();
 
-    //{
-    //    peripherals.CLOCK.tasks_hfclkstart.write(|w| unsafe {w.bits(1)});
-    //    while peripherals.CLOCK.events_hfclkstarted.read().bits() == 0 {}
-    //}
+    {
+        peripherals.CLOCK.tasks_hfclkstart.write(|w| unsafe {w.bits(1)});
+        while peripherals.CLOCK.events_hfclkstarted.read().bits() == 0 {}
+    }
 
     let p0 = peripherals.P0.split();
 
@@ -70,11 +74,21 @@ fn main() -> ! {
         )
     };
 
-    let mut si7021 = Si7021::new(i2c);
+    let mut timer = peripherals.TIMER0.constrain();
 
     writeln!(uart, "UART initialized\n").unwrap();
 
+    let mut si7021 = Si7021::new(i2c);
+
+    // Start the timer as a periodic at 1Hz. Note that the timer is configured as a 1MHz timer.
+    let time: u32 = 1_000_000;
+    timer.start(time);
+
     loop {
+        // Wait for the timer to fire.
+        block!(timer.wait()).unwrap();
+        timer.start(time);
+
         let temperature = si7021.temperature_celsius().unwrap();
         let humidity = si7021.relative_humidity().unwrap();
         writeln!(uart, "Temp: {:.2} Humidity: {:.2}", temperature, humidity).unwrap();
